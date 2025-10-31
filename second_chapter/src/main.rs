9a18ccfe95335fe5834794cd0a473a314685e8b9
kanishk-8 <kanishkkumar222004@gmail.com>
2025-10-31 14:56:03 +0530

    new learning

--- second_chapter/src/main.rs
+++ second_chapter/src/main.rs
@@ -1,12 +1,15 @@
+use chrono::Utc;
 fn main() {
     println!("Hello, world!");
     println!("ladfj;klasdhgoha");
     println!("how are you kanishk");
     sum(3, 2);
+    let now = Utc::now();
+    println!("{}", now)
 }
 // this is a function that handles sum of two numbers
 // this is a what ?
-fn sum(a:i16,b:i16){
-    let c : i16 = a+b;
-    println!("{}",c)
+fn sum(a: i16, b: i16) {
+    let c: i16 = a + b;
+    println!("{}", c)
 }
